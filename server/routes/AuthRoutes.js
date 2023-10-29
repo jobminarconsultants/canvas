3af1d9e2bcc71e0ec0b5608ad573ae80e16bf0f8
AHMED SHAIK <ahmed@AHMEDs-MacBook-Pro.local>
2023-10-29 15:53:50 +0530

    cors issue

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,9 +1,11 @@
 import express, { Router } from 'express';
-import { SignUpController, getMyUsers } from '../controllers/AuthAdminController.js';
-import {PostCoordinates, userSignUp } from '../controllers/AuthUserController.js';
+import { LoginController, SignUpController, getMyUsers } from '../controllers/AuthAdminController.js';
+import {PostCoordinates, userLoginController, userSignUp } from '../controllers/AuthUserController.js';
 const AuthRouter=Router()
 AuthRouter.post('/admin/signup',SignUpController)
+AuthRouter.post('/admin/login',LoginController)
 AuthRouter.post('/user/signup',userSignUp)
+AuthRouter.post('user/login',userLoginController)
 AuthRouter.get('/admin/:id',getMyUsers)
 AuthRouter.post('/user/cords/:id',PostCoordinates)
 export default AuthRouter
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from  'express';
 import dotenv from 'dotenv'
+import cors from 'cors'
 import DataBaseConnection from './database/conn.js';
 import AuthRouter from './routes/AuthRoutes.js';
 const app =express()
 const PORT=1010
 dotenv.config()
+app.use(cors())
 DataBaseConnection()
 app.use(express.json())
 app.use('/main',AuthRouter)
